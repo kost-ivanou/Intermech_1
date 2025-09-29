a63edb374caae81f83353e7ecfecfa64d7c8e795
kost-ivanou <kost.ivanou@gmail.com>
2025-09-29 16:45:34 +0300

    completed task 6 strings

---
+++ Konstantin_Ivanov/Tasks/Task6_Strings/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Globalization;
+using System.Text;
+
+string[] lines = File.ReadAllLines("../../../../../Resources/check.txt", Encoding.UTF8);
+
+var lookup = lines
+            .Select(line =>
+            {
+                var parts = line.Split(';');
+                return new
+                {
+                    Product = parts[0],
+                    Date = DateTime.Parse(parts[1], CultureInfo.InvariantCulture)
+                };
+            })
+            .ToLookup(x => x.Product, x => x.Date);
+
+Console.WriteLine("Локаль пользователя:");
+CultureInfo userCulture = CultureInfo.CurrentCulture;
+PrintCheck(lookup, userCulture);
+
+Console.WriteLine("\nЛокаль en-US:");
+CultureInfo engCulture = new CultureInfo("en-US");
+PrintCheck(lookup, engCulture);
+
+static void PrintCheck(ILookup<string, DateTime> productsInfo, CultureInfo culture)
+{
+    foreach (var product in productsInfo)
+    {
+        foreach(var date in product)
+        {
+            Console.WriteLine($"{product.Key}, {date.ToString(culture)}");
+        }
+    }
+}
