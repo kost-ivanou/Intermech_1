caffee947c4b09b681aa31b014f21cc06d7db4a3
kost-ivanou <kost.ivanou@gmail.com>
2025-09-30 17:23:51 +0300

    completed first part of the task

---
+++ Konstantin_Ivanov/Tasks/Task7_XML/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml;
+
+XmlDocument doc = new XmlDocument();
+doc.Load("../../../../../Resources/TelephoneBook.xml"); 
+
+XmlNodeList phones = doc.GetElementsByTagName("Phone");
+
+foreach (XmlNode phone in phones)
+{
+    Console.WriteLine(phone.InnerText);
+}
