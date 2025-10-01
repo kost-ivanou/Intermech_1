82c95be9fae0363582aa56e72e2ac33745cc342a
kost-ivanou <kost.ivanou@gmail.com>
2025-10-01 17:15:17 +0300

    completed reflection and attributes tasks, temparature converter as additional class

---
+++ Konstantin_Ivanov/Tasks/Task_8_9_Redlection_Attributes/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Reflection;
+
+Assembly asm = Assembly.LoadFrom("TemperatureLibrary.dll");
+
+foreach (Type t in asm.GetTypes())
+{
+    Console.WriteLine($"Тип: {t.FullName}");
+    PrintTypeAttributes(t);//аттрибуты самого типа
+
+    Console.WriteLine("Чьи аттрибуты смотрим?");
+    Console.WriteLine("1 - Методы, 2 - Свойства, 3 - Поля");
+    string choice = Console.ReadLine();
+
+    BindingFlags flags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Static;
+
+    switch (choice)
+    {
+        case "1":
+            foreach (MethodInfo m in t.GetMethods(flags))
+            {
+                Console.WriteLine($"{m.ReturnType.Name} {m.Name}()");
+                PrintAttributes(m);
+            }
+            break;
+        case "2":
+            foreach (PropertyInfo p in t.GetProperties(flags))
+            {
+                Console.WriteLine($"{p.PropertyType.Name} {p.Name}");
+                PrintAttributes(p);
+            }
+            break;
+        case "3":
+            foreach (FieldInfo f in t.GetFields(flags))
+            {
+                Console.WriteLine($"{f.FieldType.Name} {f.Name}");
+                PrintAttributes(f);
+            }
+            break;
+        default:
+            Console.WriteLine("от 1 до 3");
+            break;
+    }
+}
+
+Type converterType = asm.GetType("TemperatureLibrary.TemperatureConverter");
+
+object converterInstance = Activator.CreateInstance(converterType);
+
+MethodInfo method = converterType.GetMethod("CelsiusToFahrenheit");
+
+Console.Write("Введите температуру в Цельсиях: ");
+double celsius = double.Parse(Console.ReadLine());
+
+object result = method.Invoke(converterInstance, [celsius]);
+
+Console.WriteLine($"{celsius} °C = {result} °F");
+
+static void PrintAttributes(MemberInfo member)
+{
+    object[] attrs = member.GetCustomAttributes(false);
+    foreach (object attr in attrs)
+    {
+        Console.WriteLine($"[Атрибут: {attr}]");
+    }
+}
+
+static void PrintTypeAttributes(Type type)
+{
+    object[] attrs = type.GetCustomAttributes(false);
+    foreach (object attr in attrs)
+    {
+        Console.WriteLine($"[Атрибут: {attr}]");
+    }
+}
---
+++ Konstantin_Ivanov/Tasks/TemperatureConverter/TemperatureConverter.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+
+namespace TemperatureLibrary;
+
+[Serializable]
+public class TemperatureConverter
+{
+    [Required]
+    private double multiplier { get; set; }
+
+    [DebuggerStepThrough]
+    public TemperatureConverter()
+    {
+        multiplier = 9 / 5;
+    }
+
+    public double CelsiusToFahrenheit(double celsius)
+    {
+        return (celsius * multiplier) + 32;
+    }
+
+    [Obsolete("Устаревший метод")]
+    public double CeliusToFahrenheitOld(double temp)
+    {
+        return (temp * (temp - 32));
+    }
+}
