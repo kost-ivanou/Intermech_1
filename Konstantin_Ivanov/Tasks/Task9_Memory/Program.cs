f1905291133a4ca17f723ab897ff7b18079ec2b2
Konstantin <kost.ivanou@gmail.com>
2025-10-04 18:40:49 +0300

    added solution to task 9 memory

---
+++ Konstantin_Ivanov/Tasks/Task9_Memory/Program.cs
@@ -0,0 +1,24 @@
+﻿using Task9_Memory;
+
+long memoryLimitMB = 0;
+double warningLimit = 0;
+
+try
+{
+    Console.Write("Введите лимит памяти в MB: ");
+    long.TryParse(Console.ReadLine(), out memoryLimitMB);
+
+    Console.Write("Введите порог предупреждения (например, 0.8 = 80%): ");
+    double.TryParse(Console.ReadLine(), out warningLimit);
+}
+catch(Exception e)
+{
+    Console.WriteLine("Введите корректные значения");
+}
+
+var monitor = new ResourceMonitor(memoryLimitMB, warningLimit);
+
+Console.WriteLine(monitor.CheckMemoryUsage());
+
+byte[] data = new byte[50 * 1024 * 1024];
+Console.WriteLine(monitor.CheckMemoryUsage());
---
+++ Konstantin_Ivanov/Tasks/Task9_Memory/ResourceMonitor.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+namespace Task9_Memory;
+
+public class ResourceMonitor
+{
+    private readonly long memoryLimit;   
+    private readonly double warningLimit; 
+
+    public ResourceMonitor(long _memoryLimit, double _warningLimit)
+    {
+        memoryLimit = _memoryLimit * 1024 * 1024; 
+        warningLimit = _warningLimit;
+    }
+
+    public string CheckMemoryUsage()
+    {
+        Process currentProcess = Process.GetCurrentProcess();
+        long usedMemory = currentProcess.PrivateMemorySize64;
+
+        double usagePercent = (double)usedMemory / memoryLimit;
+
+        if (usagePercent >= 1.0)
+        {
+            return $"Превышен лимит! Использовано {usedMemory / (1024 * 1024)} MB " +
+                   $"из {memoryLimit / (1024 * 1024)} MB.";
+        }
+        else if (usagePercent >= warningLimit)
+        {
+            return $"Предупреждение: Использовано {usedMemory / (1024 * 1024)} MB " +
+                   $"({usagePercent:P0}) из {memoryLimit / (1024 * 1024)} MB.";
+        }
+        else
+        {
+            return $"Использование памяти в норме: {usedMemory / (1024 * 1024)} MB.";
+        }
+    }
+}
