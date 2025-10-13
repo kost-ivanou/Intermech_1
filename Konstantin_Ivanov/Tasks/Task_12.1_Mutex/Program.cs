b11d05ad4e0eb1f2d797f9c7adc636b21ed84838
Konstantin <kost.ivanou@gmail.com>
2025-10-13 14:18:33 +0300

    added solution to kernel sync tasks

---
+++ Konstantin_Ivanov/Tasks/Task_12.1_Mutex/Program.cs
@@ -0,0 +1,13 @@
+﻿bool createdNew;
+
+using (Mutex mutex = new Mutex(true, "Global\\singleapp", out createdNew))
+{
+    if (!createdNew)
+    {
+        Console.WriteLine("Приложение уже запущено");
+        return;
+    }
+
+    Console.WriteLine("Нажмите enter для выхода.");
+    Console.ReadLine();
+}
---
+++ Konstantin_Ivanov/Tasks/Task_12.2_Events/Program.cs
@@ -0,0 +1,37 @@
+﻿ManualResetEvent manualEvent = new ManualResetEvent(false);
+AutoResetEvent autoEvent = new AutoResetEvent(false);
+
+Thread manualThread = new Thread(ManualWorker);
+Thread autoThread = new Thread(AutoWorker);
+
+manualThread.Start();
+autoThread.Start();
+
+
+Console.WriteLine("\nглавный поток подает сигнал мануалу(все потоки)");
+manualEvent.Set();
+Thread.Sleep(2000);
+
+Console.WriteLine("сбросили мануал\n");
+manualEvent.Reset();
+
+Thread.Sleep(1000);
+
+Console.WriteLine("главный поток дает сигнал авто(один поток)");
+autoEvent.Set();
+Console.WriteLine("сразу и закрыли");
+Thread.Sleep(2000);
+
+void ManualWorker()
+{
+    Console.WriteLine("ManualWorker: ожидает сигнал...");
+    manualEvent.WaitOne();
+    Console.WriteLine("ManualWorker: получил сигнал и выполняет работу!");
+}
+
+void AutoWorker()
+{
+    Console.WriteLine("AutoWorker: ожидает сигнал...");
+    autoEvent.WaitOne();
+    Console.WriteLine("AutoWorker: получил сигнал и выполняет работу!");
+}
