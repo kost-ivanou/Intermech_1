3bc075058ad7c6b46d6f9a8ee1c0cce34cd6c4bc
Konstantin <kost.ivanou@gmail.com>
2025-10-13 14:58:09 +0300

    added solution to task 13 async

---
+++ Konstantin_Ivanov/Tasks/Task_13_Async/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+
+        Action<string> work = msg => Console.WriteLine(msg);
+
+        string success = "Успешно";
+
+        IAsyncResult result = work.BeginInvoke(input, ar => {
+            Console.WriteLine($"Callback получил сообщение {ar.AsyncState}");
+        }, success);
+
+        work.EndInvoke(result);
+    }
+}
