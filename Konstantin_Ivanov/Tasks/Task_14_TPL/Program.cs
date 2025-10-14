a73e1428ddee13ace3d6b757e926e7482663d9ab
Konstantin <kost.ivanou@gmail.com>
2025-10-14 18:41:19 +0300

    added solution to task 14 TPL

---
+++ Konstantin_Ivanov/Tasks/Task_14_TPL/Program.cs
@@ -0,0 +1,42 @@
+﻿class Program
+{
+    static async Task Main()
+    {
+        var parallelTasks = Task.Run(() =>
+        {
+            Parallel.Invoke(
+                MethodA,
+                MethodB
+            );
+        });
+
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine($"Главный поток, i = {i}");
+            Thread.Sleep(700);
+        }
+
+        await parallelTasks;
+        Console.WriteLine("Главный поток завершил работу");
+    }
+
+    static void MethodA()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine($"Метод A, i = {i}");
+            Thread.Sleep(700);
+        }
+        Console.WriteLine("Метод A завершён.");
+    }
+
+    static void MethodB()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            Console.WriteLine($"Метод B, i = {i}");
+            Thread.Sleep(600);
+        }
+        Console.WriteLine("Метод B завершён.");
+    }
+}
