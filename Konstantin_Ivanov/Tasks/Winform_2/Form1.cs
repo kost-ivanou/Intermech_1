6d6a6a81d13340929bd21524a96f1754a61779c9
kost-ivanou <kost.ivanou@gmail.com>
2025-10-27 23:53:15 +0300

    added solution to task 2 winform

---
+++ Konstantin_Ivanov/Tasks/Winform_2/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_2
+{
+    public partial class Form1: Form
+    {
+        private readonly Regex doubleSymbolRegex = new Regex(@"(.)\1");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            int cursorPos = textBox1.SelectionStart;
+
+            if (doubleSymbolRegex.IsMatch(textBox1.Text))
+            {
+                textBox1.Text = doubleSymbolRegex.Replace(textBox1.Text, "$1");
+                textBox1.SelectionStart = cursorPos - 1;
+            }
+        }
+    }
+}
---
+++ Konstantin_Ivanov/Tasks/Winform_2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("Winform_2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Winform_2")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("c97a3b65-47f4-438c-a69c-477b7fb00b06")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
