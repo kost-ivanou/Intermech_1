529559b8b38f55351d7be53b7f9c0cec9fdec35a
kost-ivanou <kost.ivanou@gmail.com>
2025-11-03 14:06:57 +0300

    ui

---
+++ Konstantin_Ivanov/Tasks/Winform_4/DownloadItemControl.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Winform_4
+{
+    class DownloadItemControl : UserControl
+    {
+        private Label lblFileName;
+        private ProgressBar progressBar;
+        private ErrorProvider errorProvider1;
+        private Button btnPauseResume;
+        private Label lblDownloadSpeed;
+        private Label lblStatus;
+        private Button btnCancel;
+        private System.ComponentModel.IContainer components;
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblFileName = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.lblDownloadSpeed = new System.Windows.Forms.Label();
+            this.btnPauseResume = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.btnCancel = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblFileName
+            // 
+            this.lblFileName.AutoSize = true;
+            this.lblFileName.Location = new System.Drawing.Point(13, 10);
+            this.lblFileName.Name = "lblFileName";
+            this.lblFileName.Size = new System.Drawing.Size(35, 13);
+            this.lblFileName.TabIndex = 0;
+            this.lblFileName.Text = "label1";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(16, 26);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(116, 13);
+            this.progressBar.TabIndex = 1;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // lblDownloadSpeed
+            // 
+            this.lblDownloadSpeed.AutoSize = true;
+            this.lblDownloadSpeed.Location = new System.Drawing.Point(138, 26);
+            this.lblDownloadSpeed.Name = "lblDownloadSpeed";
+            this.lblDownloadSpeed.Size = new System.Drawing.Size(35, 13);
+            this.lblDownloadSpeed.TabIndex = 2;
+            this.lblDownloadSpeed.Text = "label2";
+            // 
+            // btnPauseResume
+            // 
+            this.btnPauseResume.Location = new System.Drawing.Point(17, 45);
+            this.btnPauseResume.Name = "btnPauseResume";
+            this.btnPauseResume.Size = new System.Drawing.Size(75, 21);
+            this.btnPauseResume.TabIndex = 3;
+            this.btnPauseResume.Text = "button1";
+            this.btnPauseResume.UseVisualStyleBackColor = true;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(197, 26);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(35, 13);
+            this.lblStatus.TabIndex = 4;
+            this.lblStatus.Text = "label3";
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(98, 45);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 21);
+            this.btnCancel.TabIndex = 5;
+            this.btnCancel.Text = "button1";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // DownloadItemControl
+            // 
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnPauseResume);
+            this.Controls.Add(this.lblDownloadSpeed);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.lblFileName);
+            this.Name = "DownloadItemControl";
+            this.Size = new System.Drawing.Size(271, 76);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
---
+++ Konstantin_Ivanov/Tasks/Winform_4/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace Winform_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.добавитьФайлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.добавитьФайлToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.остановитьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.возобновитьВсеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pnlDownloads = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьФайлToolStripMenuItem1,
+            this.остановитьВсеToolStripMenuItem,
+            this.возобновитьВсеToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.добавитьФайлToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(159, 26);
+            // 
+            // добавитьФайлToolStripMenuItem
+            // 
+            this.добавитьФайлToolStripMenuItem.Name = "добавитьФайлToolStripMenuItem";
+            this.добавитьФайлToolStripMenuItem.Size = new System.Drawing.Size(158, 22);
+            this.добавитьФайлToolStripMenuItem.Text = "Добавить файл";
+            // 
+            // добавитьФайлToolStripMenuItem1
+            // 
+            this.добавитьФайлToolStripMenuItem1.Name = "добавитьФайлToolStripMenuItem1";
+            this.добавитьФайлToolStripMenuItem1.Size = new System.Drawing.Size(103, 20);
+            this.добавитьФайлToolStripMenuItem1.Text = "Добавить файл";
+            // 
+            // остановитьВсеToolStripMenuItem
+            // 
+            this.остановитьВсеToolStripMenuItem.Name = "остановитьВсеToolStripMenuItem";
+            this.остановитьВсеToolStripMenuItem.Size = new System.Drawing.Size(104, 20);
+            this.остановитьВсеToolStripMenuItem.Text = "Остановить все";
+            // 
+            // возобновитьВсеToolStripMenuItem
+            // 
+            this.возобновитьВсеToolStripMenuItem.Name = "возобновитьВсеToolStripMenuItem";
+            this.возобновитьВсеToolStripMenuItem.Size = new System.Drawing.Size(111, 20);
+            this.возобновитьВсеToolStripMenuItem.Text = "Возобновить все";
+            // 
+            // pnlDownloads
+            // 
+            this.pnlDownloads.Location = new System.Drawing.Point(12, 37);
+            this.pnlDownloads.Name = "pnlDownloads";
+            this.pnlDownloads.Size = new System.Drawing.Size(776, 401);
+            this.pnlDownloads.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pnlDownloads);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem добавитьФайлToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem остановитьВсеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem возобновитьВсеToolStripMenuItem;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem добавитьФайлToolStripMenuItem;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.FlowLayoutPanel pnlDownloads;
+    }
+}
+
